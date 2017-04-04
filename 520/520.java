a195593ec825aaf7191438f665e07953c5239b71
jackieMing <jackieming97@gmail.com>
2017-04-04 23:28:54 +0800

    520&541 done

---
+++ 520/520.java
@@ -0,0 +1,23 @@
+﻿public class Solution {
+    public boolean detectCapitalUse(String word) {
+        if(null == word || word.length() == 0)
+			return false;
+		char firstChar = word.charAt(0);
+		if(checkChar(firstChar))){
+			//要么全是大写，要么后面全是小写
+			boolean flag = true;
+			for(int i = 1;i < word.length();i++)
+				if(!checkChar(word.charAt(i))){
+					flag = false;
+					break;
+				}
+			
+			return flag;
+		}else{
+			return false;
+		}
+    }
+	public boolean checkChar(char c){
+		return c >= 'A' && c <= 'Z';
+	}
+}
---
+++ 541/541.java
@@ -0,0 +1,21 @@
+﻿public class Solution {
+    public String reverseStr(String s, int k) {
+        char[] arr = s.toCharArray();
+        int n = arr.length;
+        int i = 0;
+        while(i < n) {
+			//如果大于数组长度，则逆转最后一位
+            int j = Math.min(i + k - 1, n - 1);
+            swap(arr, i, j);
+            i += 2 * k;
+        }
+        return String.valueOf(arr);
+    }
+    private void swap(char[] arr, int l, int r) {
+        while (l < r) {
+            char temp = arr[l];
+            arr[l++] = arr[r];
+            arr[r--] = temp;
+        }
+    }
+}
