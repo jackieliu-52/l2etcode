2f72d0c10452f6499097b814f0034d57baf8f29a
jackieMing <jackieming97@gmail.com>
2017-04-04 20:57:29 +0800

    344 second way done

---
+++ 344/2.java
@@ -0,0 +1,14 @@
+﻿public class Solution {
+    public String reverseString(String s) {
+		//利用2个指针，去交换
+        char[] a = s.toCharArray();
+		int left = 0,right = s.length()-1;
+		while(left < right){
+			char temp = a[left];
+			a[left] = a[right];
+			a[right] = temp;
+			left++;right--;
+		}
+		return String.valueOf(a);
+    }
+}
